5de97420ad4ac1b9eb3ced40de1ffad256e370c7
Callebe Gomes <callebedrums@gmail.com>
2015-04-14 14:57:48 -0300

    Lesson 3 done

--- Lesson_3/00_Conference_Central/static/js/app.js
+++ Lesson_3/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '311734405024-3h3ok35vpsvitrt7k8jhcm79kbd6loth.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
