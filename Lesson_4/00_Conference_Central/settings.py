c0e68ac8aadc3c47c997d0dff42dba25a4f2801d
Callebe Gomes <callebedrums@gmail.com>
2015-04-16 14:58:12 -0300

    doing Lesson 4

--- Lesson_4/00_Conference_Central/settings.py
+++ Lesson_4/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '311734405024-3h3ok35vpsvitrt7k8jhcm79kbd6loth.apps.googleusercontent.com'
 
